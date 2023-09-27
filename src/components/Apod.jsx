1d4f2f5d057056d8dc5522a35022d58a9d6b08fb
Manug0 <mgongar12@gmail.com>
2023-09-27 14:02:53 +0200

    update width and explanation styles

--- src/components/Apod.jsx
+++ src/components/Apod.jsx
@@ -10,14 +10,14 @@ function Apod(props) {
       </div>
       <div className="Details">
         {" "}
-        <p>
-          <span className="label">Fecha:</span> {props.date}
+        <p className="Date">
+          <span>Fecha:</span> {props.date}
         </p>
-        <p>
-          <span className="label">Copyright:</span> {props.copyright}
+        <p className="Copyright">
+          <span>Copyright:</span> {props.copyright}
         </p>
-        <p>
-          <span className="label">Explicación:</span> {props.explanation}
+        <p className="Explanation">
+          <span>Explicación:</span> {props.explanation}
         </p>
       </div>
     </div>
